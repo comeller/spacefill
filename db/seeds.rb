434b481cde100da37b2d92fcb7f223770bfc14be
David Benamran <davidbenamran@gmail.com>
2018-03-13 15:20:24 +0100

    Photo uploader done

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,10 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user = User.create(email: "userfirst@gmail.com", password: "motdepasse")
+url = ["https://www.tollgroup.com/sites/default/files/styles/open_graph_image/public/images/2017-10/0770_NikeWarehouse_s1%20this%20one.jpg?itok=IvtdjyEa",
+"https://www.msdsonline.com/wp-content/uploads/warehouse.jpg",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRepmKU53XgqldC5s7x9xeVVJsOWhgXRcIMM9j0QMIska4UMRDB8w"
+]
 
  10.times do
   args_hash = {
@@ -19,12 +23,13 @@ user = User.create(email: "userfirst@gmail.com", password: "motdepasse")
     alcohol_certified: false,
     frozen_certified: true,
     transportation_services: true,
-    fullfilment_services: false,
-    devaning_services: true,
+    fulfillment_services: false,
+    devanning_services: true,
   }
 
   warehouse = Warehouse.new(args_hash)
   warehouse.user = user
+  warehouse.remote_photo_url = url.sample
   warehouse.save
 
  end
