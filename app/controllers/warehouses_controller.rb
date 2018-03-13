d899cf4e763b99dec0c84a86fa3720dee2dc17cd
Paul Ehrhardt <paul.ehrhardt@hec.edu>
2018-03-13 14:42:53 +0100

    Index: map setup + marker icons

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,11 +2,18 @@ class WarehousesController < ApplicationController
 
   def index
     @warehouses = Warehouse.all
+    @warehouses = Warehouse.where.not(latitude: nil, longitude: nil)
+    @markers = @warehouses.map do |warehouse|
+      {
+        lat: warehouse.latitude,
+        lng: warehouse.longitude
+      }
+    end
   end
 
   def show
     @warehouse = Warehouse.find(params[:id])
     @booking = Booking.new
   end
-  
+
 end
