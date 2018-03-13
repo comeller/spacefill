91c48ee66460f634dc40d398875e5a12585a11e5
Guilhem Robert <guilhem.robert@hec.edu>
2018-03-13 16:24:58 +0100

    enhanced index page and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,25 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user = User.create(email: "userfirst@gmail.com", password: "motdepasse")
+user1 = User.create(email: "userfirst@gmail.com", password: "motdepasse")
+user2 = User.create(email: "usersecond@gmail.com", password: "motdepasse")
+user3 = User.create(email: "userthird@gmail.com", password: "motdepasse")
+user4 = User.create(email: "userfourth@gmail.com", password: "motdepasse")
 
- 10.times do
+
+ 20.times do
   args_hash = {
-    address: "16 villa Gaudelet, Paris",
-    surface: 300,
-    pallets: 50,
-    description: "A great storage space in central Paris with professionnal services",
-    public_price: 15000,
-    food_grade_certified: true,
-    alcohol_certified: false,
-    frozen_certified: true,
-    transportation_services: true,
-    fullfilment_services: false,
-    devaning_services: true,
+    address: Faker::Address.street_address + ', ' + Faker::Address.city,
+    surface: rand(100..500),
+    pallets: rand(30..200),
+    description: Faker::Lorem.paragraph(6),
+    public_price: rand(500..1500),
+    food_grade_certified: [true, false].sample,
+    alcohol_certified: [true, false].sample,
+    frozen_certified: [true, false].sample,
+    transportation_services: [true, false].sample,
+    fullfilment_services: [true, false].sample,
+    devaning_services: [true, false].sample,
   }
 
   warehouse = Warehouse.new(args_hash)
-  warehouse.user = user
+  warehouse.user = [user1, user2, user3, user4].sample
   warehouse.save
 
  end
+
