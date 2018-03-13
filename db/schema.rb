e2309d2d0fe1f6b0e8bee66e14364b2ab09acb80
David Benamran <davidbenamran@gmail.com>
2018-03-13 18:14:24 +0100

    added photo per user and fixed seed for photo user

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312163146) do
-
+ActiveRecord::Schema.define(version: 20180313163324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +45,7 @@ ActiveRecord::Schema.define(version: 20180312163146) do
     t.datetime "updated_at", null: false
     t.string "company"
     t.string "phone_number"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,21 @@
+Warehouse.destroy_all
+User.destroy_all
+
 user1 = User.create(email: "userfirst@gmail.com", password: "motdepasse")
 user2 = User.create(email: "usersecond@gmail.com", password: "motdepasse")
 user3 = User.create(email: "userthird@gmail.com", password: "motdepasse")
-user4 = User.create(email: "userfourth@gmail.com", password: "motdepasse")
 
-url = ["https://www.tollgroup.com/sites/default/files/styles/open_graph_image/public/images/2017-10/0770_NikeWarehouse_s1%20this%20one.jpg?itok=IvtdjyEa",
-"https://www.msdsonline.com/wp-content/uploads/warehouse.jpg",
-"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRepmKU53XgqldC5s7x9xeVVJsOWhgXRcIMM9j0QMIska4UMRDB8w"
+user1.remote_photo_url = "https://res.cloudinary.com/dixy9tipv/image/upload/v1520958309/wozlfc0ypqwwsi9npaia.png"
+user2.remote_photo_url = "https://res.cloudinary.com/dixy9tipv/image/upload/v1520958292/iiadhxkv2z6f3xypw92v.jpg"
+user3.remote_photo_url = "https://res.cloudinary.com/dixy9tipv/image/upload/v1520958275/zdoeone2bug1s2v6dgn1.png"
+
+user1.save
+user2.save
+user3.save
+
+urls = ["https://res.cloudinary.com/dixy9tipv/image/upload/v1520960396/mahadjnu4wzx0jzteyph.jpg",
+"https://res.cloudinary.com/dixy9tipv/image/upload/v1520960395/kw3yhll0voa2migquueb.jpg",
+"https://res.cloudinary.com/dixy9tipv/image/upload/v1520960394/xzkfmly7sano2b3ofz8s.jpg"
 ]
 
  20.times do
@@ -19,13 +29,13 @@ url = ["https://www.tollgroup.com/sites/default/files/styles/open_graph_image/pu
     alcohol_certified: [true, false].sample,
     frozen_certified: [true, false].sample,
     transportation_services: [true, false].sample,
-    fullfilment_services: [true, false].sample,
-    devaning_services: [true, false].sample,
+    fulfillment_services: [true, false].sample,
+    devanning_services: [true, false].sample,
   }
 
   warehouse = Warehouse.new(args_hash)
-  warehouse.user = [user1, user2, user3, user4].sample
-  warehouse.remote_photo_url = url.sample
+  warehouse.user = [user1, user2, user3].sample
+  warehouse.remote_photo_url = urls.sample
   warehouse.save
 
  end
