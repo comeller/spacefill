4badb02cf3ef62a0dd9b7bc04c03cb2978f3a927
Paul Ehrhardt <paul.ehrhardt@hec.edu>
2018-03-14 15:39:25 +0100

    dashboard set for current user with cards as sum up of bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,7 @@
 class BookingsController < ApplicationController
 
   def my_bookings
-    @bookings = Booking.all
-    # @bookings = Booking.where(user: current_user)
+    @bookings = Booking.where(user: current_user)
     authorize :booking
   end
 end
