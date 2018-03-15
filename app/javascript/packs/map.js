7bb5b6b4b3ba73afffd6965dd5f8766b6bf81510
Guilhem Robert <guilhem.robert@hec.edu>
2018-03-15 18:29:00 +0100

    cards in map

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -6,7 +6,6 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
   const markers = JSON.parse(mapElement.dataset.markers);
   // Snazzy maps style
-  console.log(markers)
   const styles = [
     {
         "featureType": "administrative.country",
@@ -21,11 +20,17 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
         ]
     }
   ]
+
+
+
+
   map.addMarkers(markers);
   map.addStyle({
     styles: styles,
     mapTypeId: 'map_style'
   });
+
+
   map.setStyle('map_style');
   if (markers.length === 0) {
     map.setZoom(2);
